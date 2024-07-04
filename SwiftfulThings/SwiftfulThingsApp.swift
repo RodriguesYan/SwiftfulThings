bf7ca7e5ffcd38954b66b1ad0c7897f01f436ab2
Yan Rodrigues <yan.rodrigues@avenue.us>
2024-07-04 20:15:57 -0300

    Initial Commit

---
+++ SwiftfulThings/SwiftfulThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThingsApp.swift
+//  SwiftfulThings
+//
+//  Created by Yan Rodrigues on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
